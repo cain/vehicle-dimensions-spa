ad5a46face00c80635d198a4b43d0783c5e73faf
Cain Hall <cain.hall98@gmail.com>
2018-05-07 13:31:35 +1000

    Init :zap:

---
+++ README.md
@@ -0,0 +1,17 @@
+# vehicle-dimensions-spa
+
+> Vuejs vehicle dimensions spa
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn install
+
+# development at localhost:8080
+yarn dev
+
+# build for production with minification
+yarn build
+```
+
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    Vehicle Dimensions VueJS spa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB",Arial,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
